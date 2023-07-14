22187295797ab60fbfd6d6a2505a44b15ef57881
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-14 15:05:59 +0300

    finish server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connected successfully');
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
