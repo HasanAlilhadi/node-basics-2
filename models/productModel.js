a0ca03b8b8eb1dc47a95d750b0f427eb5d79f2c3
Hasan Alilhadi <hasan07703014900@gmail.com>
2023-07-14 15:05:06 +0300

    change productWeight from String to Number

--- models/productModel.js
+++ models/productModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   productName: String,
-  productPrice: String,
-  productWeight: String,
+  productPrice: Number,
+  productWeight: Number,
   productSummary: String,
 });
 const Product = mongoose.model('product', productSchema);
